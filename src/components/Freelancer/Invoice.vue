c021545f1a471508b482f3d112350b2805731d0d
Saitama <600613plusplus@gmail.com>
2019-12-04 11:18:29 +0900

    freelancer

--- src/components/Freelancer/Invoice.vue
+++ src/components/Freelancer/Invoice.vue
@@ -1,28 +1,87 @@
 <template>
   <div>
-    <h3>Накладная:
-      <span>
-        {{ content.invoiceNo }}
-      </span>
-      <div v-if="updateLoading" class="spinner">
-        <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    <div class="data-table-wrapper">
+      <div>
+        <div class="flex">
+          <DataTable
+            :headers="headers"
+            :items="items"
+            table-width="100%"
+            table-class="table-fixed"
+            thead-class="text-accent2"
+          >
+            <template v-slot:top>
+              <div class="flex">
+                <div
+                  :style="{
+                    width: fixedHeadersWidth + 'px',
+                    background: 'linear-gradient(to top, #1c1c1c 50%, #1c1c1c 100%)'
+                  }"
+                ></div>
+                <ul
+                  class="tabs"
+                  @click="switchTab"
+                >
+                  <li
+                    v-for="tab in tabs"
+                    :key="tab.value"
+                    :value="tab.value"
+                    :style="{ width: tab.width + 'px' }"
+                    :class="['tab-item', {'tab-item--active': activeTab === tab.value}]"
+                  >{{ tab.text }}</li>
+                </ul>
+              </div>
+            </template>
+            <template v-slot:items="{ items }">
+              <Product
+                v-for="(item, index) in items"
+                :key="item.id"
+                :item="item"
+                :index="index"
+                :active-tab="activeTab"
+                :profit-type="invoiceItem.profitType"
+                :profit-for-all="invoiceItem.profitForAll"
+                class="items base-accent3"
+              />
+            </template>
+
+            <template v-slot:footer>
+              <tr class="items base-accent3">
+                <td colspan="3"></td>
+                <td class="text-primary" colspan="2"></td>
+
+                <template v-if="activeTab === 1">
+                  <td class="text-gray-dark">{{ $t('shipping.total') }}</td>
+                  <td class="text-gray-dark text-center">
+                    {{ $n(invoiceItem.totalNet, 'formatted') }} <span class="text-gray-dark">{{ $t('measure.kg') }}</span>
+                  </td>
+                  <td class="text-gray-dark text-center">
+                    {{ $n(invoiceItem.totalGross, 'formatted') }} <span class="text-gray-dark">{{ $t('measure.kg') }}</span>
+                  </td>
+                  <td class="text-gray-dark text-center">
+                    {{ $n(invoiceItem.totalCapacity, 'formatted') }} <span class="text-gray-dark">{{ $t('measure.m') }}<sup>3</sup></span>
+                  </td>
+                  <td></td>
+                </template>
+
+                <template v-else>
+                  <td colspan="4"></td>
+                </template>
+
+              </tr>
+            </template>
+
+          </DataTable>
+        </div>
       </div>
-    </h3>
-    <Product
-      v-for="(item, index) in content.products"
-      :key="index"
-      :content="item"
-    />
-    <div>
-      Purchase date: {{ content.purchaseDate }}
-      Сумма: {{ content.totalPrice }}
     </div>
   </div>
 </template>
 
 <script>
-import Product from './Product.vue'
 import invoice from '../../mixins/invoice'
+
+import Product from './Product.vue'
 
 export default {
   name: 'FreelancerInvoice',
@@ -30,5 +89,82 @@ export default {
     Product,
   },
   mixins: [invoice],
+  props: {
+    invoice: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    tabs () {
+      return [
+        { value: 1, text: this.$t('shipping.warehouse'), width: 176 },
+        { value: 2, text: this.$t('shipping.description'), width: 175 },
+        { value: 3, text: this.$t('shipping.link'), width: 175 },
+      ]
+    },
+    headers () {
+      switch (this.activeTab) {
+        case 1: return [...this.productHeaders, ...this.storeHeaders]
+        case 2: return [...this.productHeaders, ...this.infoHeaders]
+        case 3: return [...this.productHeaders, ...this.linkHeaders]
+        default: return []
+      }
+    },
+    productHeaders () {
+      return [
+        { text: '', value: 'status', align: 'left', width: 14 },
+        { text: this.$t('shipping.productIndexNo'), value: 'index', align: 'left', width: 54 },
+        { text: this.$t('shipping.photo'), value: 'photo', width: 55 },
+        { text: this.$t('shipping.name'), value: 'name', width: 165 },
+        { text: this.$t('shipping.model'), value: 'model', width: 165 },
+        { text: this.$t('shipping.qty'), value: 'qty', width: 110 },
+      ]
+    },
+    storeHeaders () {
+      return [
+        { text: `${this.$t('shipping.net')}, ${this.$t('measure.kg')}`, value: 'net', width: 167, bgcolor: 'gray-darkest' },
+        { text: `${this.$t('shipping.gross')}, ${this.$t('measure.kg')}`, value: 'gross', width: 167, bgcolor: 'gray-darkest' },
+        { text: `${this.$t('shipping.packageSize')} (${this.$t('measure.mm')})`, value: 'size', width: 169, bgcolor: 'gray-darkest' },
+        { text: '', value: 'action', width: 20, bgcolor: 'gray-darkest' },
+      ]
+    },
+    infoHeaders () {
+      return [
+        { text: this.$t('shipping.additionalPhoto'), value: 'images', width: 85, bgcolor: 'gray-darkest' },
+        { text: this.$t('shipping.additionalInfo'), value: 'description', align: 'left', bgcolor: 'gray-darkest' },
+        { text: '', value: 'action', width: 20, bgcolor: 'gray-darkest' },
+      ]
+    },
+    linkHeaders () {
+      return [
+        { text: this.$t('shipping.linkField'), value: 'url', align: 'left', bgcolor: 'gray-darkest' },
+        { text: this.$t('shipping.openLink'), value: 'openLink', width: 70, bgcolor: 'gray-darkest' },
+        { text: '', value: 'action', width: 20, bgcolor: 'gray-darkest' },
+      ]
+    },
+  },
 }
 </script>
+
+<style lang="postcss" scoped>
+/* table tabs */
+.tabs {
+  height: 26px;
+  display: flex;
+  color: #434343
+}
+
+.tab-item {
+  @apply flex items-center justify-center;
+  @apply rounded-t-lg text-sm cursor-pointer;
+  bacground-color: #252525;
+}
+.tab-item:not(:first-child) {
+  margin-left:  1px;
+}
+.tab-item--active {
+  @apply bg-gray-darkest;
+  color: #ababab
+}
+</style>
--- src/components/Freelancer/Spec.vue
+++ src/components/Freelancer/Spec.vue
@@ -1,39 +1,211 @@
 <template>
-  <div>
-    <h1>
-      Спецификация:
-      <span>
-        {{ spec.specNo }}
-      </span>
-      <div v-if="updateLoading" class="spinner">
-        <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  <div class="container container--sm">
+    <div class="pt-10">
+      <div class="flex justify-between">
+        <span class="mb-3">
+          <span>{{ $t('shipping.shippingTitle') }}</span>&nbsp;
+          <span class="text-primary">
+            {{ spec.specNo }}
+          </span>&nbsp;
+          <span>{{ $t('preposition.from') }}:</span>&nbsp;
+          <span>
+            {{ $d($parseISO(spec.createdAt), 'short') }}
+          </span> /
+          <span>{{ $t('shipping.shippingClient') }}:</span>&nbsp;
+          <span>{{ specClient.uid || '' }}</span>&nbsp;
+          <span class="text-primary">
+            <Select
+              :value="specClient"
+              :placeholder="$t('placeholder.emptyText')"
+              :nudge-bottom="28"
+              :search.sync="clientSearch"
+              :items="clients"
+              searchable
+              item-value="id"
+              item-text="name"
+              solo
+              outlined
+              borderless
+              hide-details
+              class="leading-normal max-w-sm w-auto inline-flex spec-search-input"
+              style="min-width: 220px"
+              @input="updateSpec({
+                client: $event
+              })"
+            />
+          </span>
+        </span>
+        <span
+          class="text-gray text-sm cursor-pointer whitespace-no-wrap"
+          @click="collapseAll"
+        >{{ $t('action.collapseAll') }}</span>
       </div>
-    </h1>
-    Estimate Shipping date: {{ spec.estimateShippingDate }}
-    <Invoice
-      v-for="(item, index) in spec.invoices"
-      :key="index"
-      :content="item"
-    />
-    <br>
-    <template v-if="!$apollo.queries.getSpec.loading">
-      <div>
-        Итого: {{ spec.totalPrice }}
+
+      <div v-for="(item) in items" :key="item.id" class="mb-6">
+        <InvoiceHeader
+          :item="item"
+          :expanded="expanded"
+          has-icon-text
+          @click="expand"
+        >
+          <div class="flex flex-col md:flex-row pr-2 w-full md:w-auto">
+            <TextField
+              :value="item.number"
+              :debounce="250"
+              :placeholder="$t('shipping.invoiceNo')"
+              solo
+              outlined
+              colored
+              hide-details
+              class="text-sm w-40 mr-2 md:p-0 leading-normal"
+              @input="updateInvoice({
+                id: item.id, number: $event
+              })"
+            />
+            <v-menu
+              ref="menu"
+              v-model="menuPurchaseDate"
+              transition="scale-transition"
+              min-width="290px"
+              offset-y
+            >
+              <template v-slot:activator="{ on }">
+                <div v-on="on">
+                  <TextField
+                    :value="formatDate(purchaseDate)"
+                    :placeholder="$t('shipping.purchaseDate')"
+                    solo
+                    outlined
+                    colored
+                    readonly
+                    hide-details
+                    class="text-sm w-40 mr-2 md:p-0 leading-normal"
+                    @input="updateInvoice({
+                      id: item.id, purchaseDate: $event && $event.toISOString() || null
+                    })"
+                  />
+                </div>
+              </template>
+              <v-date-picker
+                v-model="purchaseDate"
+                :locale="$i18n.locale"
+                :next-icon="icons.mdiChevronRight"
+                :prev-icon="icons.mdiChevronLeft"
+                color="#5a8199"
+                no-title
+                dark
+                @change="menuPurchaseDate = false"
+              ></v-date-picker>
+            </v-menu>
+            <!-- TODO on real api, need send id -->
+            <Select
+              :value="getInvoiceSupplierName(item)"
+              :placeholder="$t('shipping.supplierName')"
+              :nudge-bottom="23"
+              :search.sync="supplierSearch"
+              :items="suppliers"
+              searchable
+              item-value="id"
+              item-text="name"
+              solo
+              outlined
+              hide-details
+              class="text-sm mr-2 md:p-0 leading-normal max-w-sm"
+              @input="updateInvoice({
+                id: item.id, invoiceSupplierId: ($event && $event.id), supplier: $event
+              })"
+            >
+              <template v-slot:prepend>
+                <a
+                  href="#"
+                  @click.prevent.stop="openCreateSupplierDialog(item)"
+                >
+                  <Icon color="#5a8199">
+                    {{ icons.mdiPlusCircleOutline }}
+                  </Icon>
+                </a>
+              </template>
+            </Select>
+            <v-menu
+              ref="menu"
+              v-model="menuShippingDate"
+              transition="scale-transition"
+              min-width="290px"
+              offset-y
+            >
+              <template v-slot:activator="{ on }">
+                <div v-on="on">
+                  <TextField
+                    :value="formatDate(shippingDate)"
+                    :placeholder="$t('shipping.shippingDate')"
+                    solo
+                    outlined
+                    colored
+                    readonly
+                    hide-details
+                    class="text-sm w-40 mr-2 md:p-0 leading-normal"
+                    @input="updateInvoice({
+                      id: item.id, shippingDate: $event && $event.toISOString() || null
+                    })"
+                  />
+                </div>
+              </template>
+              <v-date-picker
+                v-model="shippingDate"
+                :locale="$i18n.locale"
+                :next-icon="icons.mdiChevronRight"
+                :prev-icon="icons.mdiChevronLeft"
+                color="#5a8199"
+                no-title
+                dark
+                @change="menuShippingDate = false"
+              ></v-date-picker>
+            </v-menu>
+          </div>
+        </InvoiceHeader>
+        <Invoice
+          v-if="expanded.includes(item.id)"
+          style="margin-top: 1px"
+          :invoice="item"
+        />
       </div>
-    </template>
+    </div>
+    <Button
+      outline
+      class="mt-6"
+    >
+      <template v-slot:icon>
+        <Icon>{{ icons.mdiPlusCircleOutline }}</Icon>
+      </template>
+      <span>{{ $t('shipping.addInvoice') }}</span>
+    </Button>
 
   </div>
 </template>
 
 <script>
+import InvoiceHeader from './InvoiceHeader.vue'
 import Invoice from './Invoice.vue'
+
 import spec from '../../mixins/spec'
 
 export default {
   name: 'FreelancerSpec',
   components: {
+    InvoiceHeader,
     Invoice,
   },
   mixins: [spec],
+  data () {
+    return {
+      //
+    }
+  },
+  computed: {
+    //
+  },
+  methods: {
+    //
+  },
 }
 </script>
