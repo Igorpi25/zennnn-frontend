b9a5ca9ea41975a75811b18e28d07ee97e924516
Stepan Sotnikov <sotnikovse1207@gmail.com>
2020-01-14 06:34:21 +0900

    app version based on git-describe process.env.FRONT_VERSION

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
+const webpack = require('webpack')
+const { execSync } = require('child_process')
+const version = execSync('git describe --always').toString().trim()
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'FRONT_VERSION': JSON.stringify(version)
+        }
+      })
+    ]
+  },
   pluginOptions: {
     i18n: {
       locale: 'ru',
