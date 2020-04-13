fbbf34acbd29891a52f42a2eff464a4842105d89
Stepan Sotnikov <sotnikovse1207@gmail.com>
2020-04-13 16:27:22 +0900

    fix: number intl polyfill

--- src/main.js
+++ src/main.js
@@ -22,3 +22,21 @@ new Vue({
   apolloProvider,
   render: h => h(App),
 }).$mount('#app')
+
+// Intl polyfill https://github.com/andyearnshaw/Intl.js
+async function initIntl () {
+  if (typeof Intl === 'object') {
+    return
+  }
+  await Promise.all([
+    import(/* webpackChunkName: "intl" */ 'intl'),
+    import(/* webpackChunkName: "intl" */ 'intl/locale-data/jsonp/ru'),
+    import(/* webpackChunkName: "intl" */ 'intl/locale-data/jsonp/en'),
+    import(/* webpackChunkName: "intl" */ 'intl/locale-data/jsonp/fr'),
+    import(/* webpackChunkName: "intl" */ 'intl/locale-data/jsonp/uk'),
+    import(/* webpackChunkName: "intl" */ 'intl/locale-data/jsonp/zh-Hans'),
+    import(/* webpackChunkName: "intl" */ 'intl/locale-data/jsonp/zh-Hant'),
+  ])
+}
+
+initIntl()
