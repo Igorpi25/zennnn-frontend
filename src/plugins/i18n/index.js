23be12e936b4e1c405e5d6c555f7c0ba4094c762
Stepan Sotnikov <sotnikovse1207@gmail.com>
2020-06-18 11:51:03 +0900

    fix: vue-i18n pluralizationRules

--- src/plugins/i18n/index.js
+++ src/plugins/i18n/index.js
@@ -2,38 +2,6 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import { CURRENT_LANG_STORE_KEY } from '../../config/globals'
 
-const defaultImpl = VueI18n.prototype.getChoiceIndex
-
-/**
- * @param choice {number} a choice index given by the input to $tc: `$tc('path.to.rule', choiceIndex)`
- * @param choicesLength {number} an overall amount of available choices
- * @returns a final choice index to select plural word by
-**/
-VueI18n.prototype.getChoiceIndex = function (choice, choicesLength) {
-  // this === VueI18n instance, so the locale property also exists here
-  if (this.locale !== 'ru' && this.locale !== 'uk') {
-    // proceed to the default implementation
-    return defaultImpl.apply(this, arguments)
-  }
-
-  if (choice === 0) {
-    return 0
-  }
-
-  const teen = choice > 10 && choice < 20
-  const endsWithOne = choice % 10 === 1
-
-  if (!teen && endsWithOne) {
-    return 1
-  }
-
-  if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
-    return 2
-  }
-
-  return (choicesLength < 4) ? 2 : 3
-}
-
 Vue.use(VueI18n)
 
 function loadLocaleMessages () {
@@ -206,4 +174,57 @@ export default new VueI18n({
   messages: loadLocaleMessages(),
   dateTimeFormats,
   numberFormats,
+  // Key - language to use the rule for, `'ru'`, in this case
+  // Value - function to choose right plural form
+  pluralizationRules: {
+    /**
+     * @param choice {number} a choice index given by the input to $tc: `$tc('path.to.rule', choiceIndex)`
+     * @param choicesLength {number} an overall amount of available choices
+     * @returns a final choice index to select plural word by
+     */
+    ru: function (choice, choicesLength) {
+      // this === VueI18n instance, so the locale property also exists here
+
+      if (choice === 0) {
+        return 0
+      }
+
+      const teen = choice > 10 && choice < 20
+      const endsWithOne = choice % 10 === 1
+
+      if (choicesLength < 4) {
+        return (!teen && endsWithOne) ? 1 : 2
+      }
+      if (!teen && endsWithOne) {
+        return 1
+      }
+      if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
+        return 2
+      }
+
+      return (choicesLength < 4) ? 2 : 3
+    },
+  },
+  uk: function (choice, choicesLength) {
+    // this === VueI18n instance, so the locale property also exists here
+
+    if (choice === 0) {
+      return 0
+    }
+
+    const teen = choice > 10 && choice < 20
+    const endsWithOne = choice % 10 === 1
+
+    if (choicesLength < 4) {
+      return (!teen && endsWithOne) ? 1 : 2
+    }
+    if (!teen && endsWithOne) {
+      return 1
+    }
+    if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
+      return 2
+    }
+
+    return (choicesLength < 4) ? 2 : 3
+  },
 })
