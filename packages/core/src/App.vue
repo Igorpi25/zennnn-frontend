a1906af79edb461a764d10f643e0c416074c3259
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-01-22 16:41:52 +0900

    feat: added `@zennnn/core` package
    internal core components package

---
+++ packages/core/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-blue-500">
+    Playground
+  </h1>
+</template>
--- vetur.config.js
+++ vetur.config.js
@@ -1,6 +1,7 @@
 /** @type {import('vls').VeturConfig} */
 module.exports = {
   projects: [
+    './packages/core',
     './packages/zennnn',
   ]
 }
