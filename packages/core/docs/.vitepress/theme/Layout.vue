cc652a0d594fc3f276a073ee9512e70e84b0107a
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-01-30 07:17:42 +0900

    chore(core): added Logo, NavBar, toggle button

--- packages/core/docs/.vitepress/theme/Layout.vue
+++ packages/core/docs/.vitepress/theme/Layout.vue
@@ -1,27 +1,19 @@
 <template>
-  <header class="nav-bar sticky top-0 bg-white z-10 h-12 flex items-center border-b border-light-gray-400 px-6">
-    <Icon
-      class="md:hidden hover:text-gray-200 active:text-gray-200 mr-3"
-      @click="toggleMenu"
-    >
-      {{ ziMenu }}
-    </Icon>
-    <a href="/" class="font-semibold text-xl">
-      {{ $site.title }}
-    </a>
-  </header>
-  <SideBar :open="isOpen" />
-  <div
-    v-if="isOpen"
-    class="sidebar-mask md:hidden fixed top-0 left-0 w-screen h-screen"
-    @click="isOpen = false"
-  />
-  <main class="flex-grow md:ml-64 xl:ml-72 xl:mr-72 p-6">
-    <div class="max-w-screen-md mx-auto">
-      <Content />
-    </div>
-  </main>
-  <nav id="toc" class="hidden lg:block w-64 xl:w-72 fixed py-6 px-4"></nav>
+  <div>
+    <NavBar @toggle="toggleMenu" />
+    <SideBar :open="isOpen" />
+    <div
+      v-if="isOpen"
+      class="sidebar-mask md:hidden fixed top-0 left-0 w-screen h-screen"
+      @click="toggleMenu(false)"
+    />
+    <main class="flex-grow md:ml-64 xl:ml-72 xl:mr-72 p-6">
+      <div class="max-w-screen-md mx-auto">
+        <Content />
+      </div>
+    </main>
+    <nav id="toc" class="hidden lg:block w-64 xl:w-72 fixed py-6 px-4"></nav>
+  </div>
 
   <Debug />
 </template>
@@ -29,7 +21,7 @@
 <script setup>
 import { computed, ref, watch } from 'vue'
 import { useRoute } from 'vitepress'
-import { ziMenu } from '@zennnn/icons'
+import NavBar from './components/NavBar.vue'
 import SideBar from './components/SideBar.vue'
 
 const route = useRoute()
@@ -38,7 +30,7 @@ const isOpen = ref(false)
 
 watch(() => route.path, () => { isOpen.value = false })
 
-function toggleMenu () {
-  isOpen.value = !isOpen.value
+function toggleMenu (to) {
+  isOpen.value = typeof to === 'boolean' ? to : !isOpen.value
 }
 </script>
---
+++ packages/core/docs/.vitepress/theme/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="nav-bar sticky top-0 bg-white z-10 h-12 flex items-center border-b border-light-gray-400 px-6">
+    <SideBarToggleButton @toggle="$emit('toggle')" />
+    <a :href="$withBase('/')">
+      <Logo />
+    </a>
+  </header>
+</template>
+
+<script setup>
+import { defineEmit } from 'vue'
+import Logo from './Logo.vue'
+import SideBarToggleButton from './SideBarToggleButton.vue'
+
+defineEmit(['toggle'])
+</script>
