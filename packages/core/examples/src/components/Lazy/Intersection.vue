d1cfac9502efe63b9af25485defc16b2f8baec3b
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-02-09 03:38:42 +0900

    chore(core): VScroll & Intersection api data

---
+++ packages/core/examples/src/components/Lazy/Intersection.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="overflow-y-auto" style="max-height: 300px">
+    <Intersection
+      v-for="index in 60"
+      :key="index"
+      :once="false"
+      :quiet="false"
+      class="h-8"
+      transition="slide-x-transition"
+    >
+      <div class="flex items-center">
+        {{ index }}
+      </div>
+    </Intersection>
+  </div>
+</template>
---
+++ packages/core/examples/src/components/Lazy/VScroll.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <VScroll
+      :bench="0"
+      :item-height="32"
+      :items="items"
+      :height="300"
+    >
+      <template v-slot:default="{ item, index }">
+        <div>
+          {{ index }} - {{ item.text }}
+        </div>
+      </template>
+    </VScroll>
+  </div>
+</template>
+
+<script setup>
+const items = Array.from(Array(100).keys()).map(key => {
+  const i = key + 1
+  return {
+    value: `item${i}`,
+    text: `Item ${i}`,
+  }
+})
+</script>
