312e704d48a10e6ffc05ebf75663ed30d1ecd858
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-06-14 06:45:14 +0900

    fix(docs): vitepress plugin options mv

--- packages/core/docs-ssr/plugins/markdownToVue.ts
+++ packages/core/docs-ssr/plugins/markdownToVue.ts
@@ -1,17 +1,46 @@
-import { Plugin } from 'vite'
-import { SiteConfig, resolveSiteData } from 'vitepress/dist/node/config'
+import path from 'path'
+import glob from 'globby'
+import { resolveSiteData } from 'vitepress/dist/node/config'
+import { SITE_DATA_ID, SITE_DATA_REQUEST_PATH } from 'vitepress/dist/node/alias'
 import { createMarkdownToVueRenderFn } from 'vitepress/dist/node/markdownToVue'
-import { SITE_DATA_REQUEST_PATH } from 'vitepress/dist/node/alias'
-import { OutputAsset, OutputChunk } from 'rollup'
+import type { Plugin, ResolvedConfig } from 'vite'
+import type { SiteConfig, SiteData } from 'vitepress'
+import type { OutputAsset, OutputChunk } from 'rollup'
+import siteData from '../src/config'
 
-export default function (
-  root: string,
-  { configPath, alias, markdown, site, pages }: SiteConfig
-): Plugin {
+const toAbsolute = (p) => path.resolve(__dirname, '..', p)
+
+const vitepressRoot = toAbsolute('src/pages')
+
+const pages = glob
+  .sync('**.md', { cwd: toAbsolute('src/pages') })
+  .map((file) => file.toLowerCase())
+
+const siteConfig: SiteConfig = {
+  root: vitepressRoot,
+  site: siteData as SiteData,
+  pages,
+  alias: [
+    {
+      find: SITE_DATA_ID,
+      replacement: SITE_DATA_REQUEST_PATH,
+    },
+  ],
+  configPath: toAbsolute('src/config.js'),
+  outDir: '',
+  tempDir: '',
+  themeDir: '',
+}
+
+export default function (root?: string): Plugin {
+  root = root || vitepressRoot
+  const { configPath, alias, markdown, site, pages } = siteConfig
+
   const markdownToVue = createMarkdownToVueRenderFn(root, markdown, pages)
 
   let siteData = site
   let hasDeadLinks = false
+  let config: ResolvedConfig
   const pageAssetsMap = {} as Record<string, string>
 
   const isPageChunk = (
@@ -29,6 +58,10 @@ export default function (
 
   const vitePressPlugin: Plugin = {
     name: 'vitepress',
+
+    configResolved(resolvedConfig) {
+      config = resolvedConfig
+    },
 
     config() {
       return {
@@ -53,7 +86,7 @@ export default function (
     transform(code, id) {
       if (id.endsWith('.md')) {
         // transform .md files into vueSrc so plugin-vue can handle it
-        const { vueSrc, deadLinks } = markdownToVue(code, id)
+        const { vueSrc, deadLinks } = markdownToVue(code, id, config.publicDir)
         if (deadLinks.length) {
           hasDeadLinks = true
         }
@@ -108,7 +141,11 @@ export default function (
       // hot reload .md files as .vue files
       if (file.endsWith('.md') && file.includes(root)) {
         const content = await read()
-        const { pageData, vueSrc } = markdownToVue(content, file)
+        const { pageData, vueSrc } = markdownToVue(
+          content,
+          file,
+          config.publicDir
+        )
 
         // notify the client to update page data
         server.ws.send({
--- packages/core/docs-ssr/vite.config.ts
+++ packages/core/docs-ssr/vite.config.ts
@@ -1,14 +1,41 @@
 import path from 'path'
-import glob from 'globby'
 import { defineConfig } from 'vite'
-import { SITE_DATA_ID, SITE_DATA_REQUEST_PATH } from 'vitepress/dist/node/alias'
-import { SiteConfig, SiteData } from 'vitepress'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import viteComponents from 'vite-plugin-components'
 import vitePressMdToVue from './plugins/markdownToVue'
-import siteData from './src/config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: 'vue-i18n',
+        replacement: 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js',
+      },
+    ],
+  },
+  plugins: [
+    vitePressMdToVue(),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    vueJsx(),
+    vueI18n({
+      include: path.resolve(__dirname, './src/plugins/i18n/locales/**'),
+    }),
+    viteComponents({
+      customComponentResolvers: [
+        (name: string) => {
+          if (components.includes(name)) {
+            return { importName: name, path: '@zennnn/core' }
+          }
+        },
+      ],
+    }),
+  ],
+})
 
 const components = [
   'Alert',
@@ -38,52 +65,3 @@ const components = [
   'Window',
   'WindowItem',
 ]
-
-const toAbsolute = (p) => path.resolve(__dirname, p)
-
-const pages = glob
-  .sync('**.md', { cwd: toAbsolute('src/pages') })
-  .map((file) => file.toLowerCase())
-
-const config: SiteConfig = {
-  root: toAbsolute('src/pages'),
-  site: siteData as SiteData,
-  pages,
-  alias: [
-    {
-      find: 'vue-i18n',
-      replacement: 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js',
-    },
-    {
-      find: SITE_DATA_ID,
-      replacement: SITE_DATA_REQUEST_PATH,
-    },
-  ],
-  configPath: toAbsolute('src/config.js'),
-  outDir: '',
-  tempDir: '',
-  themeDir: '',
-}
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vitePressMdToVue(toAbsolute('src/pages'), config),
-    vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    vueJsx(),
-    vueI18n({
-      include: path.resolve(__dirname, './src/plugins/i18n/locales/**'),
-    }),
-    viteComponents({
-      customComponentResolvers: [
-        (name: string) => {
-          if (components.includes(name)) {
-            return { importName: name, path: '@zennnn/core' }
-          }
-        },
-      ],
-    }),
-  ],
-})
