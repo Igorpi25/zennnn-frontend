050fa189842c1ca3f29cd3b18daa1f7e2db6ad49
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-06-16 10:47:30 +0900

    chore: added comment
    TODO: redirect control

--- packages/zennnn-admin/src/router/index.ts
+++ packages/zennnn-admin/src/router/index.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
+  // TODO: control redirect on CloudFront with Lamda@Edge
+  // https://aws.amazon.com/blogs/compute/implementing-default-directory-indexes-in-amazon-s3-backed-amazon-cloudfront-origins-using-lambdaedge/
+  // remove index.html with `x-amz-website-redirect-location` metadata
   {
     path: '/',
     name: 'Home',
