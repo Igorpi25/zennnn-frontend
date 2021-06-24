a86a2a60ba1edae0c97d58b0c06608af5f435d2d
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-06-25 05:17:45 +0900

    chore(zn): global jsx extend commented

--- packages/zennnn/src/globals.d.ts
+++ packages/zennnn/src/globals.d.ts
@@ -1,11 +1,11 @@
 import { VNode } from 'vue'
 
-declare global {
-  namespace JSX {
-    // tslint:disable no-empty-interface
-    interface Element extends VNode {} // eslint-disable-line
-    interface IntrinsicElements {
-      [elem: string]: any
-    }
-  }
-}
+// declare global {
+//   namespace JSX {
+//     // tslint:disable no-empty-interface
+//     interface Element extends VNode {} // eslint-disable-line
+//     interface IntrinsicElements {
+//       [elem: string]: any
+//     }
+//   }
+// }
