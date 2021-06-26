ab999e59812fdf49a3f66c4418ee5f80400a7302
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-06-26 09:23:10 +0900

    fix(shared): Logo moved to components

--- packages/shared/components/Logo.tsx
+++ packages/shared/components/Logo.tsx
@@ -1,4 +1,4 @@
-import { FunctionalComponent } from 'vue'
+import type { FunctionalComponent } from 'vue'
 
 const Logo: FunctionalComponent = () => {
   return (
--- packages/shared/components/Navbar.tsx
+++ packages/shared/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { defineComponent } from 'vue'
-import Logo from './icons/Logo'
+import Logo from './Logo'
 
 export default defineComponent({
   props: {
