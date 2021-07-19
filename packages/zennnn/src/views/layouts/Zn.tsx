38e12fbead0ffa696e13a7ec815254917f2f2010
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-07-20 04:55:06 +0900

    fix(zn): added system message handle

--- packages/zennnn/src/views/layouts/Zn.tsx
+++ packages/zennnn/src/views/layouts/Zn.tsx
@@ -1,11 +1,31 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { RouterView } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+import { ziInfoBig } from '@zennnn/icons'
+import { Btn } from '@zennnn/core'
+import Dialog from 'shared/components/Dialog'
 import ZAppBar from '@/components/core/ZAppBar'
 import Copyright from '@/components/core/Copyright'
 import Navbar from '@/components/core/Navbar'
+import { emitter } from '@/plugins'
+import { useOrgs } from '@/composables/orgs'
 
 export default defineComponent({
   setup() {
+    const { t } = useI18n()
+    const { isOwner } = useOrgs()
+
+    const systemMessage = ref('')
+    const systemMessageDialog = ref(false)
+
+    emitter.on('show-system-message', (message) => {
+      systemMessage.value =
+        message === 'ForbiddenError: Insufficient access rights'
+          ? t('systemMessageModal.needPayment')
+          : message
+      systemMessageDialog.value = true
+    })
+
     return () => (
       <>
         <ZAppBar />
@@ -18,6 +38,21 @@ export default defineComponent({
         <footer class="container h-12 text-center">
           <Copyright />
         </footer>
+
+        <Dialog
+          v-model={systemMessageDialog.value}
+          title={t('systemMessageModal.title')}
+          icon={ziInfoBig}
+        >
+          <div class="pb-4">{systemMessage.value}</div>
+          {isOwner.value && (
+            <div class="pt-4">
+              <Btn to={{ name: 'subscription' }}>
+                {t('systemMessageModal.goToPayment')}
+              </Btn>
+            </div>
+          )}
+        </Dialog>
       </>
     )
   },
