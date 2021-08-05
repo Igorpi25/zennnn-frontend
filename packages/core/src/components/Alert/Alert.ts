aa6e13db8389ca75640336501c1363d5006b5a34
Stepan Sotnikov <sotnikovse1207@gmail.com>
2021-08-05 14:20:53 +0900

    refactor(Alert): Alert props, functions

--- packages/core/src/components/Alert/Alert.ts
+++ packages/core/src/components/Alert/Alert.ts
@@ -6,14 +6,12 @@ import {
   vShow,
   cloneVNode,
   defineComponent,
-  PropType,
 } from 'vue'
-
 import { useModel, convertToUnit } from 'vue-supp'
-
 import { ziCloseDelete, ziInfoBig } from '@zennnn/icons'
-
 import Icon from '../Icon'
+
+import type { PropType } from 'vue'
 
 export default defineComponent({
   name: 'Alert',
@@ -40,22 +38,10 @@ export default defineComponent({
     mode: String as PropType<'in-out' | 'out-in' | 'default'>,
     minWidth: [String, Number],
     maxWidth: [String, Number],
-    iconClass: {
-      type: String,
-      default: '',
-    },
-    closeClass: {
-      type: String,
-      default: '',
-    },
-    contentClass: {
-      type: String,
-      default: '',
-    },
-    containerClass: {
-      type: String,
-      default: '',
-    },
+    iconClass: String,
+    closeClass: String,
+    contentClass: String,
+    containerClass: String,
   },
 
   emits: ['update:modelValue'],
@@ -63,18 +49,18 @@ export default defineComponent({
   setup(props, { slots }) {
     const internal = useModel(props, 'modelValue')
 
-    const containerClasses = computed(() => {
-      return {
-        alert__container: true,
+    const containerClasses = computed(() => [
+      'alert__container',
+      {
         'alert__container--primary': props.color === 'primary',
         'alert__container--error': props.color === 'error',
         'alert__container--warn': props.color === 'warn',
         'alert__container--success': props.color === 'success',
         'alert__container--info': props.color === 'info',
         'alert__container--closable': props.close,
-        [props.containerClass.trim()]: true,
-      }
-    })
+      },
+      props.containerClass,
+    ])
 
     const styles = computed(() => {
       const styles: Record<string, unknown> = {}
@@ -85,19 +71,15 @@ export default defineComponent({
       return styles
     })
 
-    const hasIcon = computed(() => {
-      return slots.icon || props.icon
-    })
+    const hasIcon = computed(() => slots.icon || props.icon)
 
-    const hasClose = computed(() => {
-      return slots.close || props.close
-    })
+    const hasClose = computed(() => slots.close || props.close)
 
-    const close = () => {
+    function close() {
       internal.value = false
     }
 
-    const genIcon = () => {
+    function genIcon() {
       if (slots.icon) {
         return slots.icon()
       } else if (props.icon) {
@@ -105,22 +87,24 @@ export default defineComponent({
           Icon,
           {
             size: 24,
-            class: {
-              alert__icon: true,
-              'alert__icon--error': props.color === 'error',
-              'alert__icon--info': props.color === 'info',
-              [props.iconClass.trim()]: true,
-            },
+            class: [
+              'alert__icon',
+              {
+                'alert__icon--error': props.color === 'error',
+                'alert__icon--info': props.color === 'info',
+              },
+              props.iconClass,
+            ],
           },
           {
             default: () => ziInfoBig,
           }
         )
       }
-      return null
+      return undefined
     }
 
-    const genClose = () => {
+    function genClose() {
       const closeChildren = slots.close?.() ?? []
       if (closeChildren.length === 1) {
         return cloneVNode(closeChildren[0], { onClick: close })
@@ -129,10 +113,7 @@ export default defineComponent({
           Icon,
           {
             size: 24,
-            class: {
-              alert__close: true,
-              [props.closeClass.trim()]: true,
-            },
+            class: ['alert__close', props.closeClass],
             onClick: close,
           },
           {
@@ -140,10 +121,10 @@ export default defineComponent({
           }
         )
       }
-      return null
+      return undefined
     }
 
-    const genContent = () => {
+    function genContent() {
       const children = []
 
       if (hasIcon.value) {
@@ -154,10 +135,7 @@ export default defineComponent({
         h(
           'div',
           {
-            class: {
-              alert__content: true,
-              [props.contentClass.trim()]: true,
-            },
+            class: ['alert__content', props.contentClass],
           },
           slots.default?.() ?? props.text
         )
